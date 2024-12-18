77ba9a542b6fb5cf2084d76d76f607488b101e98
Peter Hull <petermnhull@gmail.com>
2024-12-18 09:26:49 +0000

    Add placeholder test

---
+++ internal/placeholder_test.go
@@ -0,0 +1,12 @@
+package internal_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlaceholder(t *testing.T) {
+	// This is a placeholder test to make sure the package is compiled and the tests run
+	assert.True(t, true)
+}
